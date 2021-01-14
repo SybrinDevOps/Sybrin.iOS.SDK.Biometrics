bc30cb203f9257c347f4330e6f0199018ce40798
NCelliers <nico.celliers@sybrin.com>
2021-01-14 15:35:06 +0200

    Version 1.0.0

---
+++ Sybrin_iOS_Biometrics.framework/Headers/Sybrin_iOS_Biometrics.h
@@ -0,0 +1,20 @@
+//
+//  Sybrin_iOS_Biometrics.h
+//  Sybrin.iOS.Biometrics
+//
+//  Created by Default on 2020/09/29.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Sybrin_iOS_Biometrics.
+FOUNDATION_EXPORT double Sybrin_iOS_BiometricsVersionNumber;
+
+//! Project version string for Sybrin_iOS_Biometrics.
+FOUNDATION_EXPORT const unsigned char Sybrin_iOS_BiometricsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Sybrin_iOS_Biometrics/PublicHeader.h>
+
+#import "OpenCVWrapper.h"
+#import "NCNNWrapper.h"
+
